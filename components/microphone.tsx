1e9c6f184ab30b316d3f92813605a4a2500c682c
Kenneth Courtney <kenneth.courtney@gmail.com>
2024-03-31 18:27:02 -0400

    Voice Assistant Integrated 4

--- components/microphone.tsx
+++ components/microphone.tsx
@@ -71,7 +71,9 @@ export default function Microphone() {
   return (
     <>
       {isListening ? (
-        <Siriwave theme="ios9" autostart={isListening} />
+        <div onClick={stopVapi} >
+          <Siriwave theme="ios9" autostart={isListening} />
+        </div>
       ) : (
         <button
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
